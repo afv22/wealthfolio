441872812a6d0fbd51d214fa0db2f19f5e47ceee
Andrew Vagliano <andrew.vagliano1@gmail.com>
2025-11-30 09:42:09 -0500

    Remove types index examples

--- addons/rebalancer/src/types/index.ts
+++ addons/rebalancer/src/types/index.ts
@@ -1,14 +1,3 @@
-// Export your type definitions here
-// Example:
-// export interface AddonConfig {
-//   enabled: boolean;
-//   settings: Record<string, any>;
-// }
-//
-// export type AddonPageProps = {
-//   onSettingsChange: (settings: Record<string, any>) => void;
-// };
-
 export interface AllocationData {
   assetClass: string; // Using this field name for compatibility, but will store symbol/name
   current: number;
